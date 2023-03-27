81d8258c07b2e602f73f064a71d1f55ceb7e46a8
osskit-project <128634299+osskit-project@users.noreply.github.com>
2023-03-28 00:05:02 +0300

    Delete inc directory

--- inc/funcs/move.js
+++
@@ -1,9 +0,0 @@
-Array.prototype.move = function(from, to) {
-    this.splice(to, 0, this.splice(from, 1)[0]);
-    return this;
-};
-export function move(from, to) {
-    this.cards.items.move(from, to);
-}
-
-
